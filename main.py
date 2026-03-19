d449cab478ef0badf60bfa961f27a707747a35d9
USER <eugenemabansag8@gmail.com>
2026-03-19 20:59:39 +0800

    Setup customtkinter window

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 # Pydle: Python Wordle Clone
+# importing modules
+import customtkinter as ctk
+from random import randint
 
-# list to contain all words to be used
+# window size
+window_size = "600x800"
+
+ # list to contain all words to be used
 with open("words.txt") as f:
     words = [line.strip() for line in f]
 
-print(words)
+class Wordle(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        # setting up window
+        self.geometry(window_size)
+        self.title("Pydle")
+        self.resizable(False, False)
+
+if __name__ == "__main__":
+    main = Wordle()
+    main.mainloop()
